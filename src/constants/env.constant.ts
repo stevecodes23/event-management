87a1dcc4877cd24fd14a6109e40f745805d82aad
Steve Masih <stevemasih2308@gmail.com>
2025-02-26 17:54:24 +0530

    chore: env constant file added

---
+++ src/constants/env.constant.ts
@@ -0,0 +1,22 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+function getEnvVariable(name: string) {
+  const envVar = process.env[name];
+  console.log('reaching here :', envVar);
+  if (!envVar) {
+    throw new HttpException(
+      `Environment variable ${name} not found`,
+      HttpStatus.FAILED_DEPENDENCY,
+    );
+  }
+  return envVar;
+}
+export const ENV = {
+  PORT: getEnvVariable('PORT'),
+  PSQL: {
+    HOST: getEnvVariable('DB_HOST'),
+    PORT: parseInt(getEnvVariable('DB_PORT')),
+    USERNAME: getEnvVariable('DB_USERNAME'),
+    PASSWORD: getEnvVariable('DB_PASSWORD'),
+    DATABASE: getEnvVariable('DB_DATABASE'),
+  },
+};
