3b0bb6248d1aab52446ae50c6a86025bd3d6e7d3
Steve Masih <stevemasih2308@gmail.com>
2025-02-27 02:08:08 +0530

    chore: import added om booking module

--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -4,9 +4,11 @@ import { BookingService } from './booking.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../auth/entities/user.entity';
 import { Venue } from '../venue/entities/veneu.entity';
+import { Booking } from './entities/booking.entity';
+import { Event } from '../event/entities/event.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Venue, Event])],
+  imports: [TypeOrmModule.forFeature([User, Venue, Event, Booking])],
   controllers: [BookingController],
   providers: [BookingService],
 })
