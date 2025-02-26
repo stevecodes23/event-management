c34ffa8b75a9a05996bcb18958705e21a5ca4e5e
Steve Masih <stevemasih2308@gmail.com>
2025-02-26 21:06:43 +0530

    fix:config change

--- src/modules/auth/dto/signup.dto.ts
+++ src/modules/auth/dto/signup.dto.ts
@@ -20,7 +20,7 @@ export class SignUpDto {
   @IsEmail()
   email: string;
 
-  @IsOptional()
-  @IsEnum(UserRole)
-  type: UserRole;
+  // @IsOptional()
+  // @IsEnum(UserRole)
+  // type: UserRole;
 }
