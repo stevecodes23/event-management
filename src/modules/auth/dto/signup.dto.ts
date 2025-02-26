ebef2b03afc831357ba48d2d149cbd2ba7d436ab
Steve Masih <stevemasih2308@gmail.com>
2025-02-26 22:20:20 +0530

    refactor: unused code commented out

--- src/modules/auth/dto/signup.dto.ts
+++ src/modules/auth/dto/signup.dto.ts
@@ -3,10 +3,10 @@ import {
   IsNotEmpty,
   IsString,
   IsEmail,
-  IsOptional,
-  IsEnum,
+  // IsOptional,
+  // IsEnum,
 } from 'class-validator';
-import { UserRole } from '../entities/user.entity';
+// import { UserRole } from '../entities/user.entity';
 
 export class SignUpDto {
   @IsString()
