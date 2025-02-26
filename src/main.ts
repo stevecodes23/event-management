44e81003b1924be6810a957d2054a310784597bc
Steve Masih <stevemasih2308@gmail.com>
2025-02-26 16:15:38 +0530

    feat:validation and cors enabled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  });
   console.log('running on port ', process.env.PORT);
   await app.listen(process.env.PORT ?? 3000);
 }
