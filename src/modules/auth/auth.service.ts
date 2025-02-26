f693bf73cad01d754fab4816f1d4205f24f1cbf0
Steve Masih <stevemasih2308@gmail.com>
2025-02-26 18:19:09 +0530

    feat:signup route added

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { generateHash } from 'src/utils/app.utils';
+import { SignUpDto } from './dto/signup.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+  async create(signUpDto: SignUpDto) {
+    try {
+      signUpDto.password = await generateHash(signUpDto.password);
+      await this.userRepository.save(signUpDto);
+
+      return {};
+    } catch (error) {
+      if (error.status)
+        throw new HttpException(error.message, error.getStatus());
+      else
+        throw new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+    }
+  }
+}
---
+++ src/modules/auth/dto/signup.dto.ts
@@ -0,0 +1,26 @@
+import {
+  ValidationOptions,
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsEnum,
+} from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class SignUpDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Name should not be empty' } as ValidationOptions)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  type: UserRole;
+}
