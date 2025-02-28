9060b91ad11dc64c527374dc69553330dac31e88
Steve Masih <stevemasih2308@gmail.com>
2025-02-28 13:21:03 +0530

    chore: seed file for admin added

---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import { User } from '../modules/auth/entities/user.entity';
+import { ENV } from '../constants/env.constant';
+import { Venue } from '../modules/venue/entities/venue.entity';
+import { Event } from '../modules/event/entities/event.entity';
+import { Notification } from '../modules/notification/entities/notification.entity';
+import { Booking } from '../modules/booking/entities/booking.entity';
+import { EventTicket } from '../modules/event/entities/event-ticket.entity';
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: ENV.PSQL.HOST,
+  port: ENV.PSQL.PORT,
+  username: ENV.PSQL.USERNAME,
+  password: ENV.PSQL.PASSWORD,
+  database: ENV.PSQL.DATABASE,
+  entities: [User, Venue, Event, Notification, Booking, EventTicket],
+  synchronize: false,
+});
---
+++ src/database/seeds/user.seed.ts
@@ -0,0 +1,37 @@
+import { DataSource } from 'typeorm';
+import { User } from '../../modules/auth/entities/user.entity';
+import { UserRole } from '../../modules/auth/entities/user.entity';
+import { generateHash } from '../../utils/app.utils';
+import { dataSource } from '../data-source';
+
+async function seedAdmin() {
+  await dataSource.initialize();
+  console.log('Database connected');
+
+  const userRepository = dataSource.getRepository(User);
+
+  const existingAdmin = await userRepository.findOne({
+    where: { role: UserRole.ADMIN, email: 'admin@admin.com' },
+  });
+  if (existingAdmin) {
+    console.log('Admin user already exists');
+    await dataSource.destroy();
+    return;
+  }
+
+  const adminUser = new User();
+  adminUser.name = 'Admin';
+  adminUser.email = 'admin@admin.com';
+  adminUser.password = await generateHash('test');
+  adminUser.role = UserRole.ADMIN;
+
+  await userRepository.save(adminUser);
+  console.log('Admin user seeded');
+
+  await dataSource.destroy();
+}
+
+seedAdmin().catch((error) => {
+  console.error('Error seeding admin user:', error);
+  process.exit(1);
+});
